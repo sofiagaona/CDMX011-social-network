035c5bb5af50a42904905dd56a05b1aa2661c68a
Karli <nasabisan@gmail.com>
2021-07-29 13:38:53 -0500

    Agrega configuracion e implementacion de firebase para registro

--- src/main.js
+++ src/main.js
@@ -3,3 +3,16 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+const data = document.querySelector('#register');
+// const boton = document.querySelector()
+data.addEventListener('submit', (e) =>{
+e.preventDefault();
+const signupPassword = document.querySelector('#password').value; 
+console.log(signupPassword)
+const signupEmail = document.querySelector('#email').value;
+console.log(signupEmail)
+auth
+.createUserWithEmailAndPassword(signupPassword, signupEmail)
+.then((userCredential)=>{console.log('Password y email')})
+.catch(()=>{console.log('error')})
+})
