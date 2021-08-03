b5a5f3d221804b7c8669d92fced7ed98fff2fb9d
Karli <nasabisan@gmail.com>
2021-08-03 17:55:19 -0500

    Implementa diseño de alta fidelidad en HTML y CSS corrige navegacion y añade media queries

--- templates.js
+++ templates.js
@@ -5,10 +5,10 @@ const templates = {
     },
     signup: {
         path: '/sigunp',
-        template: `<section id='modal' class='modal'><form id='register' class='modal-content flex'><label for='register'>Ingresa tus datos</label><input id='username'type='text' placeholder='Ingresa tu nombre de usuario' required><input id='email' type='email' placeholder='Ingresa tu correo electronico'required><input id='password' type='password' placeholder='Contraseña nueva' required><input id='register-button' type='submit' value='Registrarse 'class='register-button'></form></section>`
+        template: `<section id='modal' class='modal'><form id='register' class='modal-content flex'><label for='register'>Ingresa tus datos</label><input id='username'type='text' placeholder='Ingresa tu nombre de usuario' required><input id='email' type='email' placeholder='Ingresa tu correo electronico'required><input id='password' type='password' placeholder='Contraseña nueva' required><input type='submit' value='Registrarse 'class='register-button'></form></section>`
     },
     login: {
         path: '/login',
-        template: `<section id='loginModal' class='modal'><form id='login' class='modal-content flex'><label for='login'>Ingresa tus datos</label><input id='loginEmail'type='email' placeholder='Ingresa tu correo electronico' required><input id='loginPassword' type='password' placeholder='Contraseña nueva' required><input id='loginButton' type='submit' value='Iniciar Sesión' class='register-button'>`
+        template: `<section id='loginModal' class='modal'><form id='login' class='modal-content flex'><label for='login'>Ingresa tus datos</label><input id='loginEmail'type='email' placeholder='Ingresa tu correo electronico' required><input id='loginPassword' type='password' placeholder='Contraseña nueva' required><input id='login-button' type='submit' value='Iniciar Sesión' class='register-button'>`
     }
 }
